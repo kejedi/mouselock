37f37d585a8bfc41d569ecebc3b657d5400a2ffd
kejedi <kejedi@icloud.com>
2024-07-17 03:18:54 -0400

    Initial Commit

---
+++ mouselock/mouselockApp.swift
@@ -0,0 +1,17 @@
+//
+//  mouselockApp.swift
+//  mouselock
+//
+//  Created by Kevin Dion on 2024-07-17.
+//
+
+import SwiftUI
+
+@main
+struct mouselockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
